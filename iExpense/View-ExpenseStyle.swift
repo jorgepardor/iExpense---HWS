2da44829981c0bf2cf577c222fc5f897c69a9684
Jorge Pardo <hi@jorgepardo.dev>
2023-11-02 15:37:19 +0100

    Complete iExpense exercise from HWS

---
+++ iExpense/View-ExpenseStyle.swift
@@ -0,0 +1,20 @@
+//
+//  View-ExpenseStyle.swift
+//  iExpense
+//
+//  Created by Jorge Pardo on 2/11/23.
+//
+
+import SwiftUI
+
+extension View {
+    func style(for item: ExpenseItem) -> some View {
+        if item.amount < 10 {
+            return self.font(.body)
+        } else if item.amount < 100 {
+            return self.font(.title3)
+        } else {
+            return self.font(.title)
+        }
+    }
+}

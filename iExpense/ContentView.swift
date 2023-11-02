98e2c3bb8096945dcc03f15ebdf77d8f6a5e695d
Jorge Pardo <hi@jorgepardo.dev>
2023-11-02 13:53:04 +0100

    Initial Commit

---
+++ iExpense/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Jorge Pardo on 2/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Jorge Pardo on 2/11/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
